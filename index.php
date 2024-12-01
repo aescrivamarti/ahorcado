15343b4c9eab4dd50069c06fd2686b8e935660cc
aescrivamarti <alesma05@floridauniversitaria.es>
2024-12-01 11:47:19 +0100

    Actualizo index.php en B, para asegurar que está igual que en A

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <?php
 session_start();
 
+// Reiniciar el juego si se solicita
+if (isset($_GET['reiniciar']) && $_GET['reiniciar'] == 1) {
+    session_destroy();
+    header("Location: index.php");
+    exit();
+}
+
 // Lista de palabras para el juego
-$palabras = ['elefante', 'jirafa', 'hipopotamo', 'rinoceronte', 'cocodrilo', 'camello', 'chimpance'];
+$palabras = ['elefante', 'jirafa', 'hipopotamo', 'rinoceronte', 'cocodrilo', 'camello', 'chimpance', 'leon', 'tigre', 'avestruz', 'zebra'];
 
 // Inicializar el juego
 if (!isset($_SESSION['palabra'])) {
@@ -38,14 +45,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['letra'])) {
 
 // Comprobar si se ha ganado o perdido
 if ($_SESSION['letras_acertadas'] == $_SESSION['palabra']) {
-    echo "¡Enhorabuena! Has ganado :) La palabra era: " . $_SESSION['palabra'] . "<br>";
-    session_destroy();
-    echo '<a href="">Jugar de nuevo</a>';
+    header("Location: victoria.php");
     exit();
 } elseif ($_SESSION['vidas'] <= 0) {
-    echo "Lo siento, has perdido :( La palabra era: " . $_SESSION['palabra'] . "<br>";
-    session_destroy();
-    echo '<a href="">Jugar de nuevo</a>';
+    header("Location: derrota.php");
     exit();
 }
 ?>
